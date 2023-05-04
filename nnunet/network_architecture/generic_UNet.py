5058d6fe45ba01f4f7d8e2bb11de3b72761bcaec
MatthiasCami8 <matthias.cami@ml6.eu>
2023-05-04 11:14:02 +0000

    Correct spelling error from seg_output to seg_outputs

--- nnunet/network_architecture/generic_UNet.py
+++ nnunet/network_architecture/generic_UNet.py
@@ -404,8 +404,8 @@ class Generic_UNet(SegmentationNetwork):
             seg_outputs.append(self.final_nonlin(self.seg_outputs[u](x)))
             #pdb.set_trace()
 
-        for i in len(seg_output):
-            torch.save(seg_output[i], f"{nnunet.config.filename}_{i}.pt")
+        for i in len(seg_outputs):
+            torch.save(seg_outputs[i], f"{nnunet.config.filename}_{i}.pt")
         
         if self._deep_supervision and self.do_ds:
             return tuple([seg_outputs[-1]] + [i(j) for i, j in
